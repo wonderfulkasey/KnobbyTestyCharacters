be7d9bda86ea1e8e9a977921957c117555433b8b
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-11 02:42:29 +0000

    added item migrates

--- db/migrate/20200511023635_create_users.rb
+++ db/migrate/20200511023635_create_users.rb
@@ -5,4 +5,5 @@ class CreateUsers < ActiveRecord::Migration[6.0]
      t.string :email
      t.string :password_digest
   end
+ end
 end
--- db/migrate/20200511023703_items.rb
+++ db/migrate/20200511023703_items.rb
@@ -1,4 +1,12 @@
 class Items < ActiveRecord::Migration[6.0]
   def change
+    def change
+    create_table :items do |t|
+      t.string  :title
+      t.string  :description
+      t.date  :date
+      t.string  :character
+      t.integer  :user_id
+    end
   end
 end
