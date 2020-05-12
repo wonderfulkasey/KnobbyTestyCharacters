111438283d776b8e764d91767cc8cd767883b53f
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-12 17:34:08 +0000

    changed app controller to index.html

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -2,32 +2,36 @@ require './config/environment'
 
 class AppController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
+  before do
+      @title = 'Catchup!'
+    end
+
+  enable :sessions
+
+  #register Sinatra::Flash
     set :views, 'app/views'
-    enable :sessions
-    set :session_secret, "timetoduel" #ENV.fetch('SESSION_SECRET')
+    set :public_folder, 'app/public'
+
+    configure :development do
+      set :session_secret, 'only_for_development'
+    end
+
+    configure :production do
+      set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+    end
+
+    get '/' do
+      erb :'index.html'
+    end
+
+
+    helpers do
+      def logged_in?
+        !!session[:user_id]
+      end
+
+      def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+      end
+    end
   end
-
-  get "/" do
-    if logged_in?
-      redirect "/users/#{current_user.id}"
-    else
-      erb :index
-    end
-end
-
-  helpers do
-    def logged_in?
-      !!session[:user_id]
-    end
-
-    def current_user
-      @current_user ||= User.find(session[:user_id])
-    end
-
-    def authorized?(id)
-      current_user.id == id
-    end
-  end
-end
